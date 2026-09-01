5690cc1b6d6f5c68daa1188ea639db7674b39799
Vladyslav Zaporozhets <ziper.step@gmail.com>
2026-09-01 09:52:28 +0200

    Setup next-intl and category tree structure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+﻿import createNextIntlPlugin from 'next-intl/plugin';
+const withNextIntl = createNextIntlPlugin();
+const nextConfig = {};
+export default withNextIntl(nextConfig);
